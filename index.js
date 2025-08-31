765d3842f0cb2922218a5cd40a73dc951f13bfb1
Maicol <pirlo1121s@gmail.com>
2025-08-30 21:04:43 -0500

    update ConnectDb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import { connectDb } from './src/config/configDb.js';
+import  dotenv  from 'dotenv';
 const app = express();
 
+dotenv.config();
 connectDb();
 app.use( express.json() );
 app.use( cors() );
--- src/config/configDb.js
+++ src/config/configDb.js
@@ -1,14 +1,34 @@
 import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017'
+const url = process.env.MONGO_URI || 'mongodb://localhost:27017/portfolio';
 
 export async function connectDb() {
-    try {
-        await mongoose.connect(url)
-        console.log('Connect mongoose');
-    } catch (error) {
-        console.log(error)
-        console.log('No se pudo conectar a la base de datos')
-    }
-    
+  try {
+    await mongoose.connect(url);
+
+    console.log('Connect DB');
+
+    // Events
+    mongoose.connection.on('connected', () => {
+      console.log(' Mongoose está conectado');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error(' Error en la conexión a MongoDB:', err);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log(' Mongoose se ha desconectado');
+    });
+
+    process.on('SIGINT', async () => {
+      await mongoose.connection.close();
+      console.log(' Conexión de MongoDB cerrada por SIGINT');
+      process.exit(0);
+    });
+
+  } catch (error) {
+    console.error(' No se pudo conectar a MongoDB:', error.message);
+    process.exit(1); // KIll
+  }
 }
