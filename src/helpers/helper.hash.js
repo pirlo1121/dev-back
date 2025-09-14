01e9b14c6c9f7a2fe4774f3a061ce6a032af278e
Maicol <pirlo1121s@gmail.com>
2025-09-14 17:38:41 -0500

    hash pass

--- src/helpers/helper.hash.js
+++ src/helpers/helper.hash.js
@@ -9,10 +9,10 @@ export async function hashPass(plain) {
     if( typeof plain !== 'string' || plain.length === 0 ){
         throw new Error ('La contraseña no puede estar vacia');
     }
+    // const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    // return bcrypt.hash(plain, salt);
+    return await bcrypt.hash(plain, SALT_ROUNDS);
 
-    const salt = await bcrypt.genSalt(SALT_ROUNDS);
-    return bcrypt.hash(plain, salt);
-    
 }
 
 export function comparePass(plain, hashed){
