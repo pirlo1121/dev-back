8eec8d1be787de6387d3c73b40612fd1a8a1dec5
Maicol <pirlo1121s@gmail.com>
2025-09-28 18:45:08 -0500

    create dockerfile and mongo atlas

--- src/config/configDb.js
+++ src/config/configDb.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const url = process.env.MONGO_URI || 'mongodb://localhost:27017/portfolio';
+const url = process.env.MONGO_URI;
 
 export async function connectDb() {
   try {
